7c4edba004646b0f2281152c782d8c01f818c2fd
ferag <aguilarf@ifca.unican.es>
2020-10-20 08:57:42 +0000

    Ready for performing tests

--- Jenkinsfile
+++ Jenkinsfile
@@ -19,6 +19,11 @@ pipeline {
                 checkout scm
             }
         }
+        stage('Testing') {
+            steps {
+                ToxEnvRun('py36')
+            }
+        }
     }
 
     post {
---
+++ tests/test_api_1.py
@@ -0,0 +1,18 @@
+import pytest
+
+@pytest.fixture
+def test_all_indicators():
+    response = {'findable': True, 'accessible': True, 'interoperable': True, 'reusable': True}
+    result = False
+    for e in response:
+        if (response[e]):
+            result = response[e]
+        else:
+            result = response[e]
+            break
+    return result
+
+
+def test_extra():
+    result = 98
+    assert result > 0
