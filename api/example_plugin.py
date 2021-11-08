e232059897104d315819f1cf813eaa00299532e3
root <root@dspace.ifca.es>
2021-11-08 11:49:57 +0000

    Plugin example

--- api/example_plugin.py
+++ api/example_plugin.py
@@ -54,15 +54,15 @@ class Example_Plugin(Evaluator):
         #You need a way to get your metadata in a similar format
         metadata_sample = [['{http://purl.org/dc/elements/1.1/}','title', 'MyTitle', None],
                            ['{http://purl.org/dc/elements/1.1/}', 'creator', 'TheCreator', None],
-                           ['{http://purl.org/dc/elements/1.1/}', 'identifier', '123456', None],
+                           ['{http://purl.org/dc/elements/1.1/}', 'identifier', 'none', None],
                            ['{http://purl.org/dc/elements/1.1/}', 'rigths', 'https://creativecommons.org/licenses/by/4.0/', None],
                            ['{http://purl.org/dc/elements/1.1/}', 'description', 'This is the description', None],
                            ['{http://purl.org/dc/elements/1.1/}', 'date', '2019-12-12', None],
                            ['{http://purl.org/dc/elements/1.1/}', 'publisher', 'Thematic Service', None]]
 
         self.metadata = pd.DataFrame(metadata_sample,
-                                     columns=['text_value',
-                                              'metadata_schema', 'element',
+                                     columns=['tmetadata_schema',
+                                              'element', 'text_value',
                                               'qualifier'])
 
         logging.debug('METADATA: %s' % (self.metadata))
