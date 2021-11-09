64e0b670e5235ebf5f6bdb1a6557e8b45dc3aef9
Fernando Aguilar <aguilarf@ifca.unican.es>
2021-11-09 10:27:57 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-# Start the first process
-./web.py &
-  
-# Start the second process
-./fair.py &
-  
-# Wait for any process to exit
-wait -n
-  
-# Exit with status of process that exited first
-exit $?
+/FAIR_eva/fair.py &
