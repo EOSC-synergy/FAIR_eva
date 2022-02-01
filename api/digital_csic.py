51143df9b445d61e2d6c5ba1c5027196c730172c
Your Name <you@example.com>
2022-02-01 07:50:14 +0000

    First release ready

--- api/digital_csic.py
+++ api/digital_csic.py
@@ -3,6 +3,7 @@
 import ast
 import configparser
 import idutils
+import json
 import logging
 import psycopg2
 import requests
@@ -58,47 +59,48 @@ class Digital_CSIC(Evaluator):
         config = configparser.ConfigParser()
         config.read('config.ini')
         logging.debug("CONFIG LOADED")
-        try:
-            self.connection = psycopg2.connect(
-                user=config['digital_csic']['db_user'],
-                password=config['digital_csic']['db_pass'],
-                host=config['digital_csic']['db_host'],
-                port=config['digital_csic']['db_port'],
-                database=config['digital_csic']['db_db'])
-            logging.debug("DB configured")
-        except Exception as error:
-            logging.error('Error while fetching data from PostgreSQL ')
-            logging.error(error)
+        self.file_list = None
+        
+        if self.id_type == 'doi' or self.id_type == 'handle':
+            api_endpoint = 'https://digital.csic.es'
+            self.metadata, self.file_list = self.get_metadata_api(api_endpoint, self.item_id, self.id_type)
+            temp_md = self.metadata.query("element == 'identifier'")
+            self.item_id = temp_md.query("qualifier == 'uri'")['text_value'].values[0]
+            if self.metadata is not None:
+                if len(self.metadata) > 0:
+                    self.access_protocols = ['http']
+        else:
+            try:
+                self.connection = psycopg2.connect(
+                    user=config['digital_csic']['db_user'],
+                    password=config['digital_csic']['db_pass'],
+                    host=config['digital_csic']['db_host'],
+                    port=config['digital_csic']['db_port'],
+                    database=config['digital_csic']['db_db'])
+                logging.debug("DB configured")
+            except Exception as error:
+                logging.error('Error while fetching data from PostgreSQL ')
+                logging.error(error)
 
-        try:
-            self.internal_id = self.get_internal_id(self.item_id,
-                                                    self.connection)
-            if self.id_type == 'doi':
-                self.handle_id = self.get_handle_id(self.internal_id,
-                                                    self.connection)
-            elif self.id_type == 'internal':
-                self.handle_id = self.get_handle_id(self.internal_id,
-                                                    self.connection)
-                self.item_id = self.handle_id
+            try:
+                self.internal_id = self.get_internal_id(self.item_id,
+                                                        self.connection)
+                if self.id_type == 'doi':
+                    self.handle_id = self.get_handle_id(self.internal_id,
+                                                        self.connection)
+                elif self.id_type == 'internal':
+                    self.handle_id = self.get_handle_id(self.internal_id,
+                                                        self.connection)
+                    self.item_id = self.handle_id
 
-            logging.debug('INTERNAL ID: %i ITEM ID: %s' % (self.internal_id,
-                          self.item_id))
+                logging.debug('INTERNAL ID: %i ITEM ID: %s' % (self.internal_id,
+                              self.item_id))
 
-            query = \
-                'SELECT metadatavalue.text_value, metadatafieldregistry.metadata_schema_id, metadatafieldregistry.element,\
-                metadatafieldregistry.qualifier FROM item, metadatavalue, metadatafieldregistry WHERE item.item_id = %s and \
-    item.item_id = metadatavalue.resource_id AND metadatavalue.metadata_field_id = metadatafieldregistry.metadata_field_id' \
-                % self.internal_id
-            cursor = self.connection.cursor()
-            cursor.execute(query)
-            self.metadata = pd.DataFrame(cursor.fetchall(),
-                                         columns=['text_value',
-                                                  'metadata_schema', 'element',
-                                                  'qualifier'])
-            logging.debug('METADATA: %s' % (self.metadata.to_string()))
-        except Exception as e:
-            logging.error('Error connecting DB')
-            logging.error(e)
+                self.metadata = self.get_metadata_db()
+                logging.debug('METADATA: %s' % (self.metadata.to_string()))
+            except Exception as e:
+                logging.error('Error connecting DB')
+                logging.error(e)
         
         config = configparser.ConfigParser()
         config.read('config.ini')
@@ -119,8 +121,159 @@ class Digital_CSIC(Evaluator):
         global _
         _ = super().translation()
 
+
+    def get_metadata_api(self, api_endpoint, item_pid, item_type):
+        if item_type == "doi":
+            md_key = "dc.identifier.doi"
+        elif item_type == "handle":
+            md_key = "dc.identifier.uri"
+            item_pid = ut.pid_to_url(item_pid, item_type)
+
+        data = {"key": md_key, "value": item_pid}
+        headers = {'accept': 'application/json', 'Content-Type': 'application/json'}
+        logging.debug("to POST: %s" % data)
+        r = requests.post(api_endpoint + '/rest/items/find-by-metadata-field', data=json.dumps(data), headers=headers, verify=False)
+        logging.debug("ID FOUND: %s" % r.text)
+        if r.status_code == 200:
+            item_id = r.json()[0]['id']
+            r = requests.get(api_endpoint + '/rest/items/%s/metadata' % item_id,
+                             headers=headers, verify=False)
+        logging.debug("ID FOUND: %s" % r.text)
+        md = []
+        for e in r.json():
+            split_term = e['key'].split('.')
+            metadata_schema = split_term[0]
+            element = split_term[1]
+            if len(split_term) > 2:
+                qualifier = split_term[2]
+            else:
+                qualifier = None
+            text_value = e['value']
+            md.append([text_value, metadata_schema, element, qualifier])
+        metadata = pd.DataFrame(md, columns=['text_value', 'metadata_schema', 'element', 'qualifier'])
+
+        r = requests.get(api_endpoint + '/rest/items/%s/bitstreams' % item_id, headers=headers, verify=False)
+        file_list = []
+        for e in r.json():
+            file_list.append([e['name'], e['name'].split('.')[-1], e['format'], api_endpoint + e['link']])
+        file_list = pd.DataFrame(file_list, columns=['name', 'extension', 'format', 'link'])
+        return metadata, file_list
+
+    def get_metadata_db(self):
+        query = \
+                'SELECT metadatavalue.text_value, metadatafieldregistry.metadata_schema_id, metadatafieldregistry.element,\
+                metadatafieldregistry.qualifier FROM item, metadatavalue, metadatafieldregistry WHERE item.item_id = %s and \
+    item.item_id = metadatavalue.resource_id AND metadatavalue.metadata_field_id = metadatafieldregistry.metadata_field_id' \
+                % self.internal_id
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        metadata = pd.DataFrame(cursor.fetchall(),
+                                 columns=['text_value',
+                                          'metadata_schema', 'element',
+                                          'qualifier'])
+        return metadata
+ 
+
     # TESTS
     #ACCESS
+    def rda_a1_03d(self):
+        """ Indicator RDA-A1-01M
+        This indicator is linked to the following principle: A1: (Meta)data are retrievable by their
+        identifier using a standardised communication protocol. More information about that
+        principle can be found here.
+        This indicator is about the resolution of the identifier that identifies the digital object. The
+        identifier assigned to the data should be associated with a formally defined
+        retrieval/resolution mechanism that enables access to the digital object, or provides access
+        instructions for access in the case of human-mediated access. The FAIR principle and this
+        indicator do not say anything about the mutability or immutability of the digital object that
+        is identified by the data identifier -- this is an aspect that should be governed by a
+        persistence policy of the data provider
+        Technical proposal:
+        Parameters
+        ----------
+        item_id : str
+            Digital Object identifier, which can be a generic one (DOI, PID), or an internal (e.g. an
+            identifier from the repo)
+        Returns
+        -------
+        points
+            A number between 0 and 100 to indicate how well this indicator is supported
+        msg
+            Message with the results or recommendations to improve this indicator
+        """
+        msg = ""
+        points = 0
+        if self.file_list is None:
+            return super().rda_a1_03d()
+        else:
+            headers = []
+            for f in self.file_list['link']:
+                try:
+                    if ut.check_url(f):
+                        res = requests.head(f, verify=False, allow_redirects=True)
+                        if res.status_code == 200:
+                            headers.append(res.headers)
+                except Exception as e:
+                        logging.error(e)
+            if len(headers) > 0:
+                msg = msg + _("\n Files can be downloaded: %s\n %s" % (headers, self.file_list['link']))
+                points = 100
+            else:
+                msg = msg + _("\n Files can not be downloaded")
+                points = 0
+        return points, msg
+
+    def rda_a1_05d(self):
+        """ Indicator RDA-A1-01M
+        This indicator is linked to the following principle: A1: (Meta)data are retrievable by their
+        identifier using a standardised communication protocol. More information about that
+        principle can be found here.
+        The indicator refers to automated interactions between machines to access digital objects.
+        The way machines interact and grant access to the digital object will be evaluated by the
+        indicator.
+        Technical proposal:
+        Parameters
+        ----------
+        item_id : str
+            Digital Object identifier, which can be a generic one (DOI, PID), or an internal (e.g. an
+            identifier from the repo)
+        Returns
+        -------
+        points
+            A number between 0 and 100 to indicate how well this indicator is supported
+        msg
+            Message with the results or recommendations to improve this indicator
+        """
+        msg = ""
+        points = 0
+        if self.file_list is None:
+            return super().rda_a1_05d()
+        else:
+            protocol = 'http'
+            number_of_files = len(self.file_list['link'])
+            accessible_files = 0
+            for f in self.file_list['link']:
+                try:
+                    res = requests.head(f, verify=False, allow_redirects=True)
+                    if res.status_code == 200:
+                        accessible_files += 1
+                        msg = msg + "\n %s" % f
+                except Exception as e:
+                        logging.error(e)
+            if accessible_files == number_of_files:
+                points = 100
+                msg = _("Data is accessible automatically via HTTP:") + msg
+            elif accessible_files == 0:
+                points = 0
+                msg = _("Files are not accessible via HTTP")
+            else:
+                points = (accessible_files * 100) / number_of_files
+                msg = _("Some of digital objects are accessible automatically via HTTP:") + msg
+
+        return points, msg
+
+
+
     def rda_a1_2_01d(self):
         """ Indicator RDA-A1-01M
         This indicator is linked to the following principle: A1.2: The protocol allows for an
@@ -169,7 +322,6 @@ class Digital_CSIC(Evaluator):
         points = 100
         msg = _("DIGITAL.CSIC preservation policy is available at: https://digital.csic.es/dc/politicas/#politica8")
         return points, msg
-
 
     def rda_r1_2_01m(self):
         """ Indicator RDA-A1-01M
