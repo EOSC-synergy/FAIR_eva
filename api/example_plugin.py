37ee4a88cf9a78f820ad97f2e4603839a9c01d17
PalomoIFCA <143794185+PalomoIFCA@users.noreply.github.com>
2023-10-03 08:45:58 +0200

    Removed extra docstring

--- api/example_plugin.py
+++ api/example_plugin.py
@@ -12,27 +12,7 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 class Example_Plugin(Evaluator):
 
-    """
-    A class used to represent an Animal
-
-    ...
-
-    Attributes
-    ----------
-    says_str : str
-        a formatted string to print out what the animal says
-    name : str
-        the name of the animal
-    sound : str
-        the sound that the animal makes
-    num_legs : int
-        the number of legs the animal has (default 4)
-
-    Methods
-    -------
-    says(sound=None)
-        Prints the animals name and what sound it makes
-    """
+   
 
     def __init__(self, item_id, oai_base=None, lang='en'):
         super().__init__(item_id, oai_base, lang)
