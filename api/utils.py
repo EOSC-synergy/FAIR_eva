bf82a3fb53077f7805fc22dce9472a793ba40e44
Iván <palomo@ifca.unican.es>
2023-12-15 09:27:11 +0100

    Updated f tests with UUID validation
    Added a better function to check UUID validation
    Included in f3_01m, f1_01m and f1_02d

--- api/utils.py
+++ api/utils.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import idutils
 import logging
+import uuid
 import pandas as pd
 import xml.etree.ElementTree as ET
 import re
@@ -611,6 +612,11 @@ def licenses_list():
     return licenses
 
 
+
+
+
+
+
 def is_uuid(id):
     points = 0
     msg = ""
@@ -626,3 +632,23 @@ def is_uuid(id):
                     return (100, "Your id is a UUID")
     else:
         return (points, msg)
+        
+       
+
+def is_valid_uuid(value):
+  try:
+     uuid_obj = uuid.UUID(value)
+     return (100, ("Your id "+str(uuid_obj)+" is a UUID"))
+  except (ValueError, TypeError):
+     return (0,"Your ID is not a UUID")
+         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
