94e314ef2fcdf144d4a334d9e6ce2083aed99b7d
Iván <palomo@ifca.unican.es>
2024-02-29 10:03:46 +0100

    mproves

--- scripts/petition.py
+++ scripts/petition.py
@@ -10,6 +10,50 @@ import json
 # You need to set the variables oaibase, ide, and repo to the ones you want, you have 2 examples at the bottom
 
 
+def calcpoints(result, print_fullscores=False):
+    keys = ["findable", "accessible", "interoperable", "reusable", "total"]
+    values = [0, 0, 0, 0, 0]
+    result_points = 0
+    weight_of_tests = 0
+    points = dict(zip(keys, values))
+    weights = dict(zip(keys, values))
+    for key in keys[:-1]:
+        g_weight = 0
+        g_points = 0
+        for kk in result[key]:
+            result[key][kk]["indicator"] = gettext(
+                "%s.indicator" % result[key][kk]["name"]
+            )
+            # print(kk)
+            if print_fullscores != False:
+                print(
+                    "In "
+                    + str(kk)
+                    + " your item has "
+                    + str(result[key][kk]["points"])
+                    + " points"
+                )
+            result[key][kk]["name_smart"] = gettext("%s" % result[key][kk]["name"])
+            # pesos
+            weight = result[key][kk]["score"]["weight"]
+            weight_of_tests += weight
+            g_weight += weight
+            result_points += result[key][kk]["points"] * weight
+            g_points += result[key][kk]["points"] * weight
+
+        points[key] = round((g_points / g_weight), 3)
+    points["total"] = round((result_points / weight_of_tests), 2)
+    # printpoints(points)
+    return points
+
+
+def printpoints(
+    points,
+):
+    for key in points.keys():
+        print("In " + str(key) + " your item has " + str(points[key]) + " points")
+
+
 def petition(
     ide,
     oai_base,
@@ -27,6 +71,11 @@ def petition(
     result = json.loads(result.text)
     result_points = 0
     weight_of_tests = 0
+    interpoints = 0
+    interweight = 0
+
+    result = result[list(result.keys())[0]]
+
     for key in result:
         g_weight = 0
         g_points = 0
@@ -34,6 +83,9 @@ def petition(
             result[key][kk]["indicator"] = gettext(
                 "%s.indicator" % result[key][kk]["name"]
             )
+
+            # print(str(result[key][kk]["indicator"])[:-10])
+            # print(str(result[key][kk]["points"]))
             result[key][kk]["name_smart"] = gettext("%s" % result[key][kk]["name"])
             # pesos
             weight = result[key][kk]["score"]["weight"]
@@ -49,13 +101,23 @@ def petition(
                 round((g_points / g_weight), 3),
             )
         )
+
     result_points = round((result_points / weight_of_tests), 2)
     print("In total your item has " + str(result_points) + " points from a 100 total")
 
+    print(calcpoints(result))
 
-ide = "208d3855-8576-483c-ae92-89502516c28e"
-oaibase = "https://www.ics-c.epos-eu.org/api/v1/resources/details"
+
+full_details = False
+# EPOS Production API
+ide = "12b76f8e-d10e-4c5d-a113-4855f6bc3435"
+oaibase = "https://www.ics-c.epos-eu.org/api/v1"
 repo = "epos"
+
+# EPOS prototype API
+# oaibase =  'https://ics-c.epos-ip.org/development/k8s-epos-deploy/dt-geo/api/v1'
+# ide = '46676a31-fa8e-4a4a-a3c0-15d689783a08'
+
 
 ide2 = "http://hdl.handle.net/10261/157765"
 oaibase2 = "http://digital.csic.es/dspace-oai/request"
