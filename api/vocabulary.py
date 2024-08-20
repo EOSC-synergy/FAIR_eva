88855bd8c68b5151cfe43a5b178e677310e7b1fd
Pablo Orviz <orviz@ifca.unican.es>
2024-08-20 12:48:04 +0200

    FAIRsharing vocabulary implementation

--- api/vocabulary.py
+++ api/vocabulary.py
@@ -3,6 +3,8 @@ import logging
 import os
 import sys
 
+import requests
+
 from fair import app_dirname, load_config
 
 logging.basicConfig(
@@ -15,7 +17,7 @@ class VocabularyConnection:
     def __init__(self, **config_items):
         self.vocabulary_name = config_items.get("vocabulary_name", "")
         self.enable_remote_check = config_items.get("enable_remote_check", False)
-        self.remote_endpoint = config_items.get("remote_endpoint", "")
+        self.remote_path = config_items.get("remote_path", "")
         self.remote_username = config_items.get("remote_username", "")
         self.remote_password = config_items.get("remote_password", "")
         self.local_path = config_items.get("local_path", "")
@@ -40,37 +42,16 @@ class VocabularyConnection:
         # Get content from remote endpoint
         error_on_request = False
         if cls.enable_remote_check:
-            logging.debug("Accessing vocabulary '%s' remotely" % cls.name)
-            headers = {}
-            if perform_login:
-                logging.debug(
-                    "Login is required for endpoint: %s" % cls.remote_endpoint
-                )
-                headers = cls._login()
-            logging.debug(
-                "Requesting vocabulary content through endpoint: %s"
-                % cls.remote_endpoint
-            )
-            response = requests.request("POST", cls.remote_endpoint, headers=headers)
-            if response.ok:
-                logging.debug(
-                    "Successfully returned content from endpoint: %s"
-                    % cls.remote_endpoint
-                )
-                content = response.json()
-            else:
-                logging.warning(
-                    "Failed to obtain records from endpoint: %s" % response.text
-                )
-                error_on_request = True
+            logger.debug("Accessing vocabulary '%s' remotely" % cls.name)
+            error_on_request, content = cls._remote_collect(cls)
         # Get content from local cache
         if not cls.enable_remote_check or error_on_request:
-            logging.debug(
+            logger.debug(
                 "Accessing vocabulary '%s' from local cache: %s"
                 % (cls.name, cls.local_path)
             )
             cls.local_path_full = os.path.join(app_dirname, cls.local_path)
-            logging.debug("Full path to local cache: %s" % cls.local_path_full)
+            logger.debug("Full path to local cache: %s" % cls.local_path_full)
             content = cls._local_collect(cls)
 
         return content
@@ -111,30 +92,8 @@ class IANAMediaTypes(VocabularyConnection):
 
 
 class FAIRsharingRegistry(VocabularyConnection):
-    def __init__(self, username, password, metadata_path, format_path):
-        self.username = username
-        self.password = password
-        self._api_url = (
-            "https://api.fairsharing.org/search/fairsharing_records?page[size]=2500&%s"
-        )
-        self.paths = {
-            "metadata_standards": {
-                "local": metadata_path,
-                "remote": self._api_url
-                % "fairsharing_registry=standard&user_defined_tags=metadata standardization",
-            },
-            "formats": {
-                "local": format_path,
-                "remote": self._api_url % "user_defined_tags=Geospatial data",
-            },
-            "serialization": {
-                "local": "",
-                "remote": self._api_url % "domains=Resource metadata",
-            },
-        }
-        self._metadata_standards = {}
-        self._formats = {}
-        self._serialization = {}
+    name = "FAIRsharing registry"
+    _config = load_config()  # FIXME: get only the properties from 'FAIRsharing' section
 
     def _login(self):
         url_api_login = "https://api.fairsharing.org/users/sign_in"
@@ -158,32 +117,62 @@ class FAIRsharingRegistry(VocabularyConnection):
                 "Authorization": "Bearer {0}".format(token),
             }
         else:
-            logging.warning(
+            logger.warning(
                 "Could not get token from FAIRsharing API: %s" % response.text
             )
 
         return headers
 
     def _remote_collect(self):
-        self._login()
+        error_on_request = False
+        content = []
+        if not (self.remote_username and self.remote_password):
+            logger.error(
+                "Could not get required 'username' and 'password' properties for accessing FAIRsharing registry API"
+            )
+        else:
+            headers = self._login(self)
+            logger.debug("Got headers from sign in process: %s" % headers)
+            response = requests.request("POST", self.remote_path, headers=headers)
+            if response.ok:
+                content = response.json().get("data", [])
+                if content:
+                    logger.debug(
+                        "Successfully returned %s items from search query: %s"
+                        % (len(content), self.remote_path)
+                    )
+                else:
+                    error_on_request = True
+            else:
+                logger.warning(
+                    "Failed to obtain records from endpoint: %s" % response.text
+                )
+                error_on_request = True
 
-    @property
-    def metadata_standards(self):
-        api_results = self._remote_or_local_query(search_item="metadata_standards")
-        self._metadata_standards = api_results
-        return self._metadata_standards
+        return error_on_request, content
 
-    @property
-    def formats(self):
-        api_results = self._remote_or_local_query(search_item="formats")
-        self._formats = api_results
-        return self._formats
+    @classmethod
+    def collect(cls, search_topic):
+        config_items = dict(cls._config.items("vocabularies:fairsharing"))
+        remote_path = config_items.get("remote_path", "")
+        if not remote_path:
+            logger.warning(
+                "Could not get FAIRsharing API endpoint from configuration (check 'remote_path' property)"
+            )
+        else:
+            query_parameter = "q=%s" % search_topic
+            remote_path_with_query = "?page[size]=2500&".join(
+                [remote_path, query_parameter]
+            )
+            config_items["remote_path"] = remote_path_with_query
+            logger.debug(
+                "Request URL to FAIRsharing API with search topic '%s': %s"
+                % (search_topic, config_items["remote_path"])
+            )
+        super().__init__(cls, **config_items)
+        content = super().collect(cls)
 
-    @property
-    def serialization(self):
-        api_results = self._remote_or_local_query(search_item="serialization")
-        self._serialization = api_results
-        return self._serialization
+        return content
 
 
 class Vocabulary:
@@ -193,5 +182,6 @@ class Vocabulary:
         return vocabulary.collect()
 
     @staticmethod
-    def get_fair_sharing():
-        return FAIRsharingRegistry.collect()
+    def get_fairsharing(search_topic):
+        vocabulary = FAIRsharingRegistry()
+        return vocabulary.collect(search_topic=search_topic)
