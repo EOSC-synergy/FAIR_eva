5dd5f557e36b2bde987dbba2c0ce5e16dc377e55
Pablo Orviz <orviz@ifca.unican.es>
2024-09-13 09:13:08 +0200

    Adapt new config reqs in missing plugins

--- plugins/digital_csic/plugin.py
+++ plugins/digital_csic/plugin.py
@@ -86,7 +86,7 @@ class Plugin(Evaluator):
     lang : Language
     """
 
-    def __init__(self, item_id, oai_base=None, lang="en"):
+    def __init__(self, item_id, oai_base=None, lang="en", config=None):
         logger.debug("Call parent")
         plugin = "digital_csic"
         super().__init__(item_id, oai_base, lang, plugin)
